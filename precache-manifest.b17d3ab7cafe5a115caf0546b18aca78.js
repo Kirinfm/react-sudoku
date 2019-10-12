f22955eb753cc4441102550799fcff5b0a93264d
Kirinfm <fangming312581573@outlook.com>
2019-10-12 16:22:59 +0800

    Updates

---
+++ precache-manifest.b17d3ab7cafe5a115caf0546b18aca78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a85392305b029a175934d4517aef3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1917c588bef2ce1af7d",
+    "url": "./static/css/main.add6f9cd.chunk.css"
+  },
+  {
+    "revision": "5506ed9fc6de733c65fe",
+    "url": "./static/js/2.a28c8114.chunk.js"
+  },
+  {
+    "revision": "c1917c588bef2ce1af7d",
+    "url": "./static/js/main.4d35f0a0.chunk.js"
+  },
+  {
+    "revision": "c6424a62d5a07b00901c",
+    "url": "./static/js/runtime-main.bdafaf6b.js"
+  }
+]);
